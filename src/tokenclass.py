3f3aeb107ac07a5f7a324d468f83d2f8910ff15c
MtScience <107070506+MtScience@users.noreply.github.com>
2022-12-29 13:29:59 +0000

    Add files via upload

---
+++ src/tokenclass.py
@@ -0,0 +1,66 @@
+from enum import Enum, auto
+
+
+class TokenType(Enum):
+    # Single-character tokens
+    LEFT_PAREN = auto()
+    RIGHT_PAREN = auto()
+    LEFT_BRACE = auto()
+    RIGHT_BRACE = auto()
+    COMMA = auto()
+    DOT = auto()
+    MINUS = auto()
+    PLUS = auto()
+    SEMICOLON = auto()
+    SLASH = auto()
+    STAR = auto()
+    CARET = auto()
+
+    # One or two character tokens
+    BANG = auto()
+    BANG_EQUAL = auto()
+    EQUAL = auto()
+    EQUAL_EQUAL = auto()
+    GREATER = auto()
+    GREATER_EQUAL = auto()
+    LESS = auto()
+    LESS_EQUAL = auto()
+
+    # Literals
+    IDENTIFIER = auto()
+    STRING = auto()
+    NUMBER = auto()
+
+    # Keywords
+    AND = auto()
+    CLASS = auto()
+    ELSE = auto()
+    FALSE = auto()
+    FUN = auto()
+    FOR = auto()
+    IF = auto()
+    NIL = auto()
+    OR = auto()
+    PRINT = auto()
+    RETURN = auto()
+    SUPER = auto()
+    THIS = auto()
+    TRUE = auto()
+    VAR = auto()
+    WHILE = auto()
+
+    EOF = auto()
+
+
+class Token:
+    def __init__(self, typ: TokenType, lexeme: str, literal: object | None, line: int):
+        self.type: TokenType = typ
+        self.lexeme: str = lexeme
+        self.literal: object | None = literal
+        self.line: int = line
+
+    def __str__(self) -> str:
+        return f"{self.type} {self.lexeme} {self.literal}"
+
+
+__all__ = ["Token", "TokenType"]
