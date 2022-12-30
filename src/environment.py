9009b22c08eee8cde2211cec5e85842143436d81
MtScience <107070506+MtScience@users.noreply.github.com>
2022-12-30 14:03:15 +0000

    Update environment.py

--- src/environment.py
+++ src/environment.py
@@ -3,8 +3,9 @@ from tokenclass import Token
 
 
 class Environment:
-    def __init__(self):
+    def __init__(self, enclosing=None):
         self.__values: dict[str, object] = {}
+        self.enclosing: Environment | None = enclosing
 
     def define(self, name: str, value: object) -> None:
         self.__values |= {name: value}
@@ -13,11 +14,18 @@ class Environment:
         if name.lexeme in self.__values:
             return self.__values.get(name.lexeme)
 
+        if self.enclosing is not None:
+            return self.enclosing.get(name)
+
         raise LoxRuntimeError(name, f"Undefined variable '{name.lexeme}'.")
 
     def assign(self, name: Token, value: object) -> None:
         if name.lexeme in self.__values:
             self.__values |= {name.lexeme: value}
+            return
+
+        if self.enclosing is not None:
+            self.enclosing.assign(name, value)
             return
 
         raise LoxRuntimeError(name, f"Undefined variable '{name.lexeme}'.")
