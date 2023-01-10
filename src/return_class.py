6e5a08841ba3a1174c81e3cf4366dd05c7a29fdc
MtScience <107070506+MtScience@users.noreply.github.com>
2023-01-10 13:57:19 +0000

    Update return_class.py

--- src/return_class.py
+++ src/return_class.py
@@ -1,7 +1,7 @@
 class Return(Exception):
     def __init__(self, value: object | None):
         super().__init__()
-        self.value = value
+        self.value: object | None = value
 
 
 __all__ = "Return"
