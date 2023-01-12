7394cdcf05801acca96a823828202ec4a25c1b65
MtScience <107070506+MtScience@users.noreply.github.com>
2023-01-12 12:41:52 +0000

    Update lox_class.py

--- src/lox_class.py
+++ src/lox_class.py
@@ -1,9 +1,27 @@
-class LoxClass:
+from lox_callable import LoxCallable
+
+
+class LoxClass(LoxCallable):
     def __init__(self, name: str):
         self.name: str = name
 
+    def call(self, interpreter, arguments: list[object]) -> object:
+        instance: LoxInstance = LoxInstance(self)
+        return instance
+
+    def arity(self) -> int:
+        return 0
+
     def __str__(self) -> str:
-        return self.name
+        return f"<class {self.name}>"
 
 
-__all__ = "LoxClass"
+class LoxInstance:
+    def __init__(self, klass: LoxClass):
+        self.__klass: LoxClass = klass
+
+    def __str__(self) -> str:
+        return f"<{self.__klass.name} instance>"
+
+
+__all__ = ["LoxClass", "LoxInstance"]
