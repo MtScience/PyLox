c94e94c74c4543ed8f689e4906eababbea284de9
MtScience <107070506+MtScience@users.noreply.github.com>
2023-04-28 12:51:03 +0300

    Update pylox.py
    Added support for command-line options "-h", "-e" and "-i"

--- src/pylox.py
+++ src/pylox.py
@@ -44,7 +44,16 @@ class Lox:
 
         self.__interpreter.interpret(statements, mode)
 
-    def run_repl(self) -> None:
+    def __run_options(self, options: dict) -> None:
+        if options is None:
+            return
+
+        if "-e" in options:
+            self.__run(options["-e"], OpMode.SCRIPT)
+
+    def run_repl(self, options: dict = None) -> None:
+        self.__run_options(options)
+
         while True:
             try:
                 line = input("> ")
@@ -55,10 +64,11 @@ class Lox:
             self.__run(line, OpMode.INTERACTIVE)
             self.had_error = False  # Unset error flag to allow for printing after errors in REPL
 
-    def run_file(self, path: str) -> None:
+    def run_file(self, path: str, options: dict = None) -> None:
         with open(path, "rt", encoding="utf-8") as file:
             code = file.read()
 
+        self.__run_options(options)
         self.__run(code, OpMode.SCRIPT)
 
         if self.had_error:
@@ -66,12 +76,56 @@ class Lox:
         if self.had_runtime_error:
             sys.exit(70)
 
+        if options is not None and "-i" in options:
+            self.run_repl()
+
+
+def print_usage() -> None:
+    usage: str = "usage: python pylox.py [options] [script]\n\n" \
+                 "Available options:\n"\
+                 " -h        show this message and exit\n"\
+                 " -e str    run string 'str'\n"\
+                 " -i        enter interactive mode\n"
+    print(usage)
+
+
+def collect_options(given: list) -> tuple[dict, str | None]:
+    i: int = 0
+    options: dict = {}
+    script: str | None = None
+
+    while i < (length := len(given)):
+        match given[i]:
+            case "-i" | "-h": options |= {given[i]: None}
+            case "-e":
+                i += 1
+                if i >= length or given[i].startswith("-"):
+                    print(f"'-e' requires an argument")
+                    print_usage()
+                    sys.exit(64)
+                options |= {"-e": given[i]}
+            case _ if given[i].startswith("-"):
+                print(f"unrecognized option: '{given[i]}'")
+                print_usage()
+                sys.exit(64)
+            case _:
+                script = given[i]
+                break
+        i += 1
+
+    return options, script
+
 
 if __name__ == "__main__":
-    if (length := len(sys.argv)) > 2:
-        print("Usage: pylox.py [script]")
-        sys.exit(64)
-    elif length == 2:
-        Lox().run_file(sys.argv[1])
+    options: dict
+    script: str | None
+    options, script = collect_options(sys.argv[1:])
+
+    if "-h" in options:
+        print_usage()
+        sys.exit()
+
+    if script is not None:
+        Lox().run_file(script, options)
     else:
-        Lox().run_repl()
+        Lox().run_repl(options)
